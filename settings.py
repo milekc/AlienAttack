d68a91ea1a71fc35008f28f0d67b9846123e7694
milekc <milekc@yandex.ru>
2022-01-08 09:23:19 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    '''Класс для хранения настроек'''
+    def __init__(self):
+        '''Иницилизация настройки игры'''
+        # Парамеры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
