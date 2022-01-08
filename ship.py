91015688705261735418554aeea9981838e2291a
milekc <milekc@yandex.ru>
2022-01-08 22:09:45 +0300

    09/01/2022

---
+++ ship.py
@@ -0,0 +1,34 @@
+import pygame
+
+
+class Ship():
+    '''Класс для управления кораблем'''
+
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        # Загружает изображение корабля и получает прямоугольник
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        # Каждый новый корабль появляется у нижнего края экрана
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Сщхранение вещественной координаты центра корабля
+        self.x = float(self.rect.x)
+
+        # Флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        # Обновился атрибут x, а не rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= self.settings.ship_speed
+
+    def blitime(self):
+        '''Рисует корабль в текущей позиции'''
+        self.screen.blit(self.image, self.rect)
