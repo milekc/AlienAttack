bf9d0f9eaa4d95a5872ea06a6225e66e0ddbb2fb
milekc <milekc@yandex.ru>
2022-01-11 23:11:23 +0300

    перемещение пришельцев из стороны в сторону

--- alien_invasion.py
+++ alien_invasion.py
@@ -87,6 +87,7 @@ class AlienInvasion:
 
     def _update_aliens(self):
         '''Обновляет позиции всех пришельцев во флоте'''
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _creat_fleet(self):
@@ -115,6 +116,20 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        '''Реагирует на достижение пришельцем края экрана'''
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        '''Опускает весь флот и меняет направление флота.'''
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
 
     def _update_screen(self):
         '''Обновляет изображения на экране и отображает новый экран'''
