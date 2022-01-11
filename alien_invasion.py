73aa3bc968607abdbbf28ece42f4febf6f4436f7
milekc <milekc@yandex.ru>
2022-01-11 22:51:33 +0300

    перемещение пришельца вправо

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -83,6 +84,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        '''Обновляет позиции всех пришельцев во флоте'''
+        self.aliens.update()
 
     def _creat_fleet(self):
         '''Создание флота вторжения'''
